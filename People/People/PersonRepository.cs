0302ef53df909dc0420fcbafaa66ff7d105d0467
babalex16 <82571265+babalex16@users.noreply.github.com>
2023-05-01 15:51:57 +0200

    Async access to sqlite implemented

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using People.Models;
 
 namespace People
 {
@@ -12,11 +14,14 @@ namespace People
 
         public string StatusMessage { get; set; }
 
-        // TODO: Add variable for the SQLite connection
+        private SQLiteAsyncConnection conn;
 
-        private void Init()
+        private async Task Init()
         {
-            // TODO: Add code to initialize the repository         
+           if (conn != null)    //ensures the initialization code for the SQLite database only will run once.
+                return;
+            conn = new SQLiteAsyncConnection(_dbPath);
+            await conn.CreateTableAsync<Person>();
         }
 
         public PersonRepository(string dbPath)
@@ -24,21 +29,20 @@ namespace People
             _dbPath = dbPath;                        
         }
 
-        public void AddNewPerson(string name)
-        {            
+        public async Task AddNewPerson(string name)
+        {
             int result = 0;
             try
             {
-                // TODO: Call Init()
+                await Init();
 
                 // basic validation to ensure a name was entered
                 if (string.IsNullOrEmpty(name))
                     throw new Exception("Valid name required");
 
-                // TODO: Insert the new person into the database
-                result = 0;
+                result = await conn.InsertAsync(new Person { Name = name });
 
-                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+                StatusMessage = string.Format("{0} record(s) added [Name: {1})", result, name);
             }
             catch (Exception ex)
             {
@@ -47,12 +51,12 @@ namespace People
 
         }
 
-        public List<Person> GetAllPeople()
+        public async Task<List<Person>> GetAllPeople()
         {
-            // TODO: Init then retrieve a list of Person objects from the database into a list
             try
             {
-                
+                await Init();
+                return await conn.Table<Person>().ToListAsync();
             }
             catch (Exception ex)
             {
